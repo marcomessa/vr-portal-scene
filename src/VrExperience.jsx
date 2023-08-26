a5e2031eaa850cd4248fc30fe9d8017809cceeab
marcomessa <marco.messa3@gmail.com>
2023-08-26 10:38:07 +0200

    chore: change indentation

--- src/VrExperience.jsx
+++ src/VrExperience.jsx
@@ -4,31 +4,32 @@ import { useThree } from "@react-three/fiber";
 import { useState } from "react";
 
 export default function VrExperience({ children }) {
-    const { gl } = useThree();
-    const [baseReferenceSpace, setBaseReferenceSpace] = useState(null);
-    const onSessionStart = () => {
-        const referenceSpace = gl.xr.getReferenceSpace();
-        setBaseReferenceSpace(referenceSpace);
+  const { gl } = useThree();
+  const [baseReferenceSpace, setBaseReferenceSpace] = useState(null);
 
-        const offsetPosition = { x: 0, y: 2, z: 0, w: 1 };
-        const offsetRotation = new THREE.Quaternion();
-        const transform = new XRRigidTransform(offsetPosition, offsetRotation);
-        const teleportSpaceOffset =
-            referenceSpace.getOffsetReferenceSpace(transform);
+  const onSessionStart = () => {
+    const referenceSpace = gl.xr.getReferenceSpace();
+    setBaseReferenceSpace(referenceSpace);
 
-        gl.xr.setReferenceSpace(teleportSpaceOffset);
-    };
+    const offsetPosition = { x: 0, y: 2, z: 0, w: 1 };
+    const offsetRotation = new THREE.Quaternion();
+    const transform = new XRRigidTransform(offsetPosition, offsetRotation);
+    const teleportSpaceOffset =
+      referenceSpace.getOffsetReferenceSpace(transform);
 
-    const onSessionEnd = () => {
-        gl.xr.setReferenceSpace(baseReferenceSpace);
-        // setBaseReferenceSpace(null);
-    };
+    gl.xr.setReferenceSpace(teleportSpaceOffset);
+  };
 
-    return (
-        <XR onSessionStart={onSessionStart} onSessionEnd={onSessionEnd}>
-            <Controllers />
-            <Hands />
-            {children}
-        </XR>
-    );
+  const onSessionEnd = () => {
+    gl.xr.setReferenceSpace(baseReferenceSpace);
+    // setBaseReferenceSpace(null);
+  };
+
+  return (
+    <XR onSessionStart={onSessionStart} onSessionEnd={onSessionEnd}>
+      <Controllers />
+      <Hands />
+      {children}
+    </XR>
+  );
 }
